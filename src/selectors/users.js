6ddbecd6ca93b41bb680ea2932e3dfa0873f2568
Caleb Elliott <caleb123elliott@gmail.com>
2023-05-25 23:34:59 -0700

    white space

--- src/selectors/users.js
+++ src/selectors/users.js
@@ -1,5 +1,4 @@
 export const setUserData = (state, payload) => ({ ...state, user: payload })
 export const getUserData = state => state.user.user
-
 export const setLoginError = (state, payload) => ({ ...state, loginError: payload })
 export const getLoginError = state => state.user.loginError
